ec4d1d739a372e1a44e2bb35a634940a4c39009f
Josep Audenis <pepaudenisr@gmail.com>
2025-08-10 18:14:23 +0200

    feed_preference.json finished

--- src/feed_extraction.py
+++ src/feed_extraction.py
@@ -2,5 +2,11 @@ import feedparser
 
 FEED_URL = "https://arxiv.org/rss/cs.LG"
 
-feed = feedparser.parse(FEED_URL)
-print(feed)
+
+def extract_feed():
+    
+    feed = feedparser.parse(FEED_URL)
+    print(feed)
+
+if __name__ == "__main__.py":
+    extract_feed()
