51495bbb08d808e52bbe01fe6390967fb0b48180
Josep Audenis <pepaudenisr@gmail.com>
2025-08-11 14:35:38 +0200

    html parsing

---
+++ src/arxiv_pipeline/paper_filter.py
@@ -0,0 +1,16 @@
+from text_fetcher import TextFetcher
+
+class PaperFilter:
+
+    THRESHOLD = 5
+
+    def __init__(self, preferences):
+        self.preferences = preferences
+
+    def filter(self, papers):
+
+        filtered = []
+
+        
+
+        return filtered
